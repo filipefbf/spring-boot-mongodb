b515ffa181da367e49e152e1c3dd8a94d9a1cc26
Filipe Barbosa Ferreira <filipefbf@ibm.com>
2022-08-04 15:34:37 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/filipeferreira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/filipeferreira/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.filipeferreira.workshopmongo.config;
 import com.filipeferreira.workshopmongo.domain.Post;
 import com.filipeferreira.workshopmongo.domain.User;
 import com.filipeferreira.workshopmongo.dto.AuthorDTO;
+import com.filipeferreira.workshopmongo.dto.CommentDTO;
 import com.filipeferreira.workshopmongo.repository.PostRepository;
 import com.filipeferreira.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem","Vou viajar para SãoPaulo Abraços", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), " Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa Viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha uma ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1));
+        post2.getComments().addAll(Arrays.asList(c2, c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/filipeferreira/workshopmongo/domain/Post.java
+++ src/main/java/com/filipeferreira/workshopmongo/domain/Post.java
@@ -1,6 +1,7 @@
 package com.filipeferreira.workshopmongo.domain;
 
 import com.filipeferreira.workshopmongo.dto.AuthorDTO;
+import com.filipeferreira.workshopmongo.dto.CommentDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +9,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -24,6 +27,16 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 
     @Override
     public boolean equals(Object o) {
