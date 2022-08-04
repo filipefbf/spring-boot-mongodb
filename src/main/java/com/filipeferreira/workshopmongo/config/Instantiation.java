1c53820b0202a07858191683d2e1835c1c0cafcc
Filipe Barbosa Ferreira <filipefbf@ibm.com>
2022-08-04 15:13:22 -0300

    Referenciando os posts do usuário

--- src/main/java/com/filipeferreira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/filipeferreira/workshopmongo/config/Instantiation.java
@@ -35,10 +35,13 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
-        
+
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem","Vou viajar para SãoPaulo Abraços", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), " Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
--- src/main/java/com/filipeferreira/workshopmongo/domain/User.java
+++ src/main/java/com/filipeferreira/workshopmongo/domain/User.java
@@ -3,14 +3,17 @@ package com.filipeferreira.workshopmongo.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class User implements Serializable {
@@ -20,6 +23,15 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
     @Override
     public boolean equals(Object o) {
