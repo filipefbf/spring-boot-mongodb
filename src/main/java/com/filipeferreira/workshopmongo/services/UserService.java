e60cc6207d7976891790b7544210a392829c525b
Filipe Barbosa Ferreira <filipefbf@ibm.com>
2022-08-04 12:50:52 -0300

    Inserção de usuário com POST

--- src/main/java/com/filipeferreira/workshopmongo/services/UserService.java
+++ src/main/java/com/filipeferreira/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.filipeferreira.workshopmongo.services;
 
 import com.filipeferreira.workshopmongo.domain.entities.User;
+import com.filipeferreira.workshopmongo.dto.UserDTO;
 import com.filipeferreira.workshopmongo.repository.UserRepository;
 import com.filipeferreira.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getName());
     }
 }
